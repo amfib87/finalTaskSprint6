3d7b93c8b1c934757ae89b91c08cdf1700c0cc69
Shatokhin Vitaliy <amfib87@gmail.com>
2025-05-24 10:45:26 +0300

    firstAttemp

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,67 @@
 package handlers
+
+import (
+	"bufio"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandleRoot(res http.ResponseWriter, req *http.Request) {
+	relativePath := "../index.html"
+
+	absPath, err := filepath.Abs(relativePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write(data)
+
+}
+
+func HandleUpload(res http.ResponseWriter, req *http.Request) {
+	req.ParseMultipartForm(10 << 20)
+
+	file, _, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(res, "error open file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	var content string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		content += scanner.Text()
+	}
+
+	content, err = service.Conversion(content)
+	if err != nil {
+		http.Error(res, "error conversion", http.StatusInternalServerError)
+		return
+	}
+
+	filename := time.Now().Format("02.01.06 15-04-05") + ".txt"
+	err = os.WriteFile(filename, []byte(content), 0755)
+	if err != nil {
+		http.Error(res, "error write local file", http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte(content))
+
+}
